725282012f8fe28b8270a93cf6f555a5ba2f63e2
Sorrowa <2791562645@qq.com>
2018-11-08 21:16:30 +0800

    第一个界面的测试版
    在Flash活动中，我们可以添加各种各样需要的初始化工作。

--- app/src/main/java/com/example/zhangzihao/secondhand/View/FlashActivity.java
+++ app/src/main/java/com/example/zhangzihao/secondhand/View/FlashActivity.java
@@ -1,6 +1,9 @@
 package com.example.zhangzihao.secondhand.View;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,8 +13,20 @@ import com.example.zhangzihao.secondhand.R;
 
 public class FlashActivity extends AppCompatActivity implements View.OnClickListener {
 
+
+    private static final int GOON = 0;
+    private static final int STOP = 1;
+
+
     //用于跳转的按钮
     private Button buttonToChange;
+
+    private Handler mhandler;
+
+    //剩余的时间
+    private int time = 3000;
+
+    private Context context=this;
 
 
     @Override
@@ -20,24 +35,55 @@ public class FlashActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_flash);
 
 
-        buttonToChange=findViewById(R.id.button);
+        buttonToChange = findViewById(R.id.button);
 
         buttonToChange.setOnClickListener(this);
 
+        //实现倒计时机制
+        mhandler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
 
+                //todo:继续下一次计时
+                int lastTime=msg.what;
+                lastTime = lastTime - 1000;
+                if (0 == lastTime) {
+                    Intent intent = new Intent(context
+                            ,MainActivity.class);
+                    context.startActivity(intent);
+                    finish();
+                    return true;
+                }else {
+                    Message message=new Message();
+                    message.what=lastTime;
+                    mhandler.sendMessageDelayed(message,1000);
+                    final int finalLastTime = lastTime;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            buttonToChange.setText((finalLastTime /1000)+"s后自动跳转");
+                        }
+                    });
+                    return true;
+                }
+            }
+        });
 
+        //启动计时
+        Message message=new Message();
+        message.what=time;
+        mhandler.sendMessageDelayed(message,1000);
 
     }
 
 
-
-
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.button:
-                Intent intent=new Intent(this,MainActivity.class);
+                Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
+                finish();
         }
     }
 }
