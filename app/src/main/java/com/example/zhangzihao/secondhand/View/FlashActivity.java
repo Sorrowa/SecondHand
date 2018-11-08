96bcd52533fdb9c8280e335666d194355688755f
Sorrowa <2791562645@qq.com>
2018-11-08 20:45:48 +0800

    first time
    主活动和开app过场活动

---
+++ app/src/main/java/com/example/zhangzihao/secondhand/View/FlashActivity.java
@@ -0,0 +1,43 @@
+package com.example.zhangzihao.secondhand.View;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.zhangzihao.secondhand.R;
+
+public class FlashActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //用于跳转的按钮
+    private Button buttonToChange;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash);
+
+
+        buttonToChange=findViewById(R.id.button);
+
+        buttonToChange.setOnClickListener(this);
+
+
+
+
+    }
+
+
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                Intent intent=new Intent(this,MainActivity.class);
+                startActivity(intent);
+        }
+    }
+}
