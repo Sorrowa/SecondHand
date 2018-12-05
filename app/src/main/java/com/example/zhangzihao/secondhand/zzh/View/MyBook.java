6d7a06f49434e9827bcc829e18ee0a74b24d92f7
Sorrowa <2791562645@qq.com>
2018-12-05 11:34:36 +0800

    第一阶段的我的图书界面

--- app/src/main/java/com/example/zhangzihao/secondhand/zzh/View/MyBook.java
+++ app/src/main/java/com/example/zhangzihao/secondhand/zzh/View/MyBook.java
@@ -2,19 +2,45 @@ package com.example.zhangzihao.secondhand.zzh.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import com.example.zhangzihao.secondhand.R;
 import com.example.zhangzihao.secondhand.zzh.Presenter.MyBookPresenter;
 
 public class MyBook extends AppCompatActivity implements BaseView<MyBookPresenter>{
 
-    //presenterdu对象
+    //presenter对象
     private MyBookPresenter presenter;
+
+    private Toolbar toolbar;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_book);
+
+        bindPresenter(new MyBookPresenter(this));
+
+        initeView();
+
+        initeRecycleView();
+
+    }
+
+    /**
+     * 初始化recycleView
+     */
+    private void initeRecycleView() {
+    }
+
+    /**
+     * 注入布局元素
+     */
+    private void initeView() {
+        toolbar=findViewById(R.id.books_toolbar);
+        recyclerView=findViewById(R.id.my_books);
     }
 
 
